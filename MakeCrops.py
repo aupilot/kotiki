eefaeee9944cd4a459a227746d89ae53d72101e5
Kirill Makukhin <info@webage.net.au>
2017-05-15 20:48:26 +1000

    первая версия scale recognition

---
+++ MakeCrops.py
@@ -0,0 +1,173 @@
+# create a set of crops for further learning
+
+import os
+import cv2
+import numpy as np
+import re, math, random, time
+from scipy.spatial.distance import cdist
+
+
+def makeSquare(thumb, lionSize):
+    """
+    pad an image to make it square
+
+    """
+    h, w, c = np.shape(thumb)
+    cv2.copyMakeBorder(thumb,
+                       math.floor((lionSize - h) / 2),
+                       math.ceil((lionSize - h) / 2),
+                       math.floor((lionSize - w) / 2),
+                       math.ceil((lionSize - w) / 2),
+                       cv2.BORDER_REFLECT_101)
+    return thumb
+
+lion_size = 224
+
+out_dir_classes = False               #  use classification dir structure (for built-in classification generatior)
+out_dir = "./train."+str(lion_size)+"/" if out_dir_classes else "./train.scale."+str(lion_size)+"/"
+
+inp_dir = '../Sealion/'
+classes = ["adult_males", "subadult_males", "adult_females", "juveniles", "pups", "negatives"]
+coords = "/Volumes/KProData/Users/kir/Documents/Programming/Python/Sealion/coords.csv"
+
+start_time = time.time()
+
+if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
+
+# load CSV
+centres = np.genfromtxt(coords, delimiter=',', skip_header=1, dtype=int)
+# centres = pd.read_csv(coords)
+
+file_names = os.listdir(inp_dir + "Train/")
+file_names = sorted(file_names, key=lambda
+    item: (int(item.partition('.')[0]) if item[0].isdigit() else float('inf'), item))
+
+ds_store = ".DS_Store"
+if ds_store in file_names: file_names.remove(ds_store)
+
+# select a subset of files to run on
+file_names = file_names[1:-1]
+
+# dataframe to store results in
+# coordinates_df = pd.DataFrame(index=file_names, columns=classes)
+
+for filename in file_names:
+    # read a Train image
+    image = cv2.imread(inp_dir + "Train/" + filename)
+    height, width, channels = np.shape(image)
+
+    print(filename + '\t', end="", flush=True)
+
+    fileNoTxt = re.findall(r'\d+', filename)
+    fileNo = int(fileNoTxt[0])
+
+    idx = (centres[:, 0] == fileNo)
+    centres1img = centres[idx]
+
+    pos = 0         # считаем positive samples
+    neg = 0         # считаем negative samples
+    num = 0         # сквозная нумерация для случая без out_dir_classes - включаем в crop file name
+
+    for lion_class in classes:
+        iii = 0
+        if lion_class != 'negatives':
+            # === positives
+            idx = (centres1img[:, 1] == classes.index(lion_class))
+            centresClass = centres1img[idx]
+
+            for cent in centresClass:
+                # in coords y -> [2], x -> [3]
+                thumb = image[cent[2] - int(lion_size / 2):cent[2] + int(lion_size / 2),
+                        cent[3] - int(lion_size / 2):cent[3] + int(lion_size / 2), :]
+
+                # if not square - discard
+                if np.shape(thumb) != (lion_size, lion_size, 3):
+                    continue
+
+                # print(np.shape(thumb))
+                # plt.imshow(thumb);plt.draw()
+                # plt.show(block=False)
+
+                if out_dir_classes:
+                    cv2.imwrite(out_dir + lion_class + str(fileNo) + "_" + str(iii) + ".jpg", thumb)
+                else:
+                    cv2.imwrite(out_dir + str(fileNo) + "_" + str(num) + ".jpg", thumb)
+
+                pos = pos + 1
+                iii = iii + 1
+                num = num + 1
+
+        else:
+            # === negatives
+            # make random crops ~ twice as much as another classes, given that they are far away from any centres
+            i = 0
+            while True:
+                # generate neg centre
+                randCent = np.array([(random.randint(int(lion_size / 2), width - int(lion_size / 2)),
+                                      random.randint(int(lion_size / 2), height - int(lion_size / 2)))])
+
+                # find the distance to all positive centres in the picture
+                allCents = np.array(centres1img[:, [2, 3]])
+                dist = cdist(allCents, randCent, 'sqeuclidean')
+
+                # if the picrture has no lions
+                if dist.size == 0:
+                    # just save few random
+                    thumb = image[randCent[0, 1] - int(lion_size / 2):randCent[0, 1] + int(lion_size / 2),
+                            randCent[0, 0] - int(lion_size / 2):randCent[0, 0] + int(lion_size / 2), :]
+
+                    if out_dir_classes:
+                        # make it square if not
+                        if np.shape(thumb) != (lion_size, lion_size, 3):
+                            thumb = makeSquare(thumb, lion_size)
+                        cv2.imwrite(out_dir + lion_class + str(fileNo) + "_" + str(iii) + ".jpg", thumb)
+                    else:
+                        # if not square - discard
+                        if np.shape(thumb) != (lion_size, lion_size, 3):
+                            continue
+                        cv2.imwrite(out_dir + str(fileNo) + "_" + str(num) + ".jpg", thumb)
+
+                    neg = neg + 1
+                    iii = iii + 1
+                    num = num + 1
+                else:
+                    # save only if not too close & not too far from positive lioms (or randomly with prob 5%)
+                    if ((np.amin(dist) > lion_size / 3) and (np.amax(dist) < (lion_size * 2) ** 2)) or random.randint(1,
+                                                                                                                      100) < 5:
+                        # print(dist)
+                        thumb = image[randCent[0, 1] - int(lion_size / 2):randCent[0, 1] + int(lion_size / 2),
+                                randCent[0, 0] - int(lion_size / 2):randCent[0, 0] + int(lion_size / 2), :]
+
+                        if out_dir_classes:
+                            # make it square if not
+                            if np.shape(thumb) != (lion_size, lion_size, 3):
+                                thumb = makeSquare(thumb, lion_size)
+                            cv2.imwrite(out_dir + lion_class + str(fileNo) + "_" + str(iii) + ".jpg", thumb)
+                        else:
+                            # if not square - discard
+                            if np.shape(thumb) != (lion_size, lion_size, 3):
+                                continue
+                            cv2.imwrite(out_dir + str(fileNo) + "_" + str(num) + ".jpg", thumb)
+
+                        neg = neg + 1
+                        iii = iii + 1
+                        num = num + 1
+
+                # don't want to stuck here forever
+                i = i + 1
+                if i > 2500:
+                    break
+                if pos != 0 and neg >= int(pos / 3):
+                    break
+                if pos == 0 and neg >= 10:
+                    break
+
+        print(".", end="", flush=True)
+    print(" Pos:", pos, " Neg:", neg)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+
+
---
+++ TrainScale.py
@@ -0,0 +1,228 @@
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Model, load_model
+from keras.layers import Dropout, Conv2D, Reshape
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+from keras.utils import plot_model
+from keras.utils.layer_utils import print_summary
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPool2D
+
+from keras.preprocessing.image import load_img, img_to_array
+
+import matplotlib.pyplot as plt
+from time import gmtime, strftime
+import os
+import re
+import numpy as np
+
+
+from kir_utils import kir_save_history
+
+# === Train a small conv net on top of vgg16 to detect lions' scale ===
+
+batch_size = 16
+epochs1    = 40
+epochs2    = 80
+
+resumeFrom=None
+#resumeFrom='cp/crops-58-0.44.hdf5'  # <= this will trigger resuming
+resumeEpochFrom=80                   # the last trained. Will resume from resumeEpochFrom+1
+resumeEpochs=120
+
+img_dir   = "train.scale.224"
+val_dir   = "train.scale.224.val"
+scale_dir = "../Sealion/TrainScale/"
+
+# img_width, img_height = 112, 112
+img_width, img_height = 224, 224
+
+
+def kir_train_generator(img_dir, scale_dir, batch_size=16):
+
+    file_names = [f for f in os.listdir(img_dir) if re.match(r'[0-9]+.*\.jpg', f)]
+    file_names = sorted(file_names, key=lambda
+        item: (int(item.partition('.')[0]) if item[0].isdigit() else float('inf'), item))
+
+    # csv = os.path.join(img_dir, 'coords.csv')
+    # all_coords = np.genfromtxt(coord_file, delimiter=',', skip_header=1)    # pic,class,row,col
+
+    # read all scales
+    scale_names = [f for f in os.listdir(scale_dir) if re.match(r'[0-9]+.*\.txt', f)]
+    scale_names = sorted(scale_names, key=lambda
+        item: (int(item.partition('_')[0]) if item[0].isdigit() else float('inf'), item))
+
+    scales = np.ones((1000,1))
+    # scales = np.ones((len(file_names),1))
+    for scale_name in scale_names:
+        # iii = int(os.path.splitext(scale_name)[0])
+        iii = int(scale_name.partition('_')[0])
+        scales[iii] = np.loadtxt(os.path.join(scale_dir, scale_name))
+
+    batchX = np.zeros((batch_size, img_width, img_height, 3))
+    batchY = np.zeros((batch_size, 1))
+    ptr = 0
+
+    while True:
+        for filename in file_names:
+            # get file number
+            iii = int(filename.partition('_')[0])
+
+            img = load_img(os.path.join(img_dir,filename))
+            xx = img_to_array(img)
+            xx = xx/128.-1.
+
+            batchX[ptr,:,:,:] = xx
+            batchY[ptr] = scales[iii]
+
+            ptr = ptr + 1
+
+            if ptr == batch_size:
+                # output
+                # yield batchX, np.expand_dims(batchY, axis=0)
+                yield batchX, batchY
+                # yield np.zeros((16,224,224,3)), np.zeros((16,1))
+                ptr = 0
+                batchY = np.zeros((batch_size, 1))
+
+# # Test generator
+# gen = kir_train_generator(img_dir=img_dir, scale_dir=scale_dir, batch_size=batch_size)
+# for j in range(15000):
+#     a, b = next(gen)
+# print(a.ndim, b.ndim)
+
+times = strftime("%Y%m%d-%H-%M-%S", gmtime())
+
+# if not os.path.isdir(out_dir):
+#     os.mkdir(out_dir)
+
+# Save the model according to the conditions
+checkpoint = ModelCheckpoint(filepath='cp/scale-{epoch:02d}-{val_loss:.2f}.hdf5',
+                             monitor='val_acc',
+                             verbose=0,
+                             save_best_only=True,
+                             save_weights_only=False,
+                             mode='auto',
+                             period=1)
+tensorboard = TensorBoard(log_dir='./logs/scale-{}'.format(times),
+                          histogram_freq=5,
+                          write_graph=False,
+                          write_images=True)
+# early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+
+# ========= Model ==========
+if resumeFrom == None:
+
+    model_pretrained = applications.VGG16(weights ="imagenet", include_top=False, input_shape=(img_width, img_height, 3))
+    print('VGG Model loaded.')
+
+    # Freeze the first 15 layers which we don't want to train
+    for layer in model_pretrained.layers[:16]:
+        layer.trainable = False
+
+    x = model_pretrained.output
+
+    x = MaxPool2D(pool_size=(7, 7), padding='same', name='Kir_0')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.25)(x)
+    x = Conv2D(512, (1, 1), activation='elu', padding='valid', name='Kir_1')(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(512, (1, 1), activation='elu', padding='valid', name='Kir_2')(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(1, (1, 1), activation='elu', padding='valid', name='Kir_3')(x)
+    predictions = Reshape(target_shape=(1,))(x)
+
+    model = Model(input=model_pretrained.input, output=predictions)
+
+    # compile the model
+    # model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    # model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adadelta(lr=0.1, rho=0.95, epsilon=1e-08, decay=0.0005), metrics=['accuracy'])
+    # model.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"], decay=0.0005)
+
+    # model.compile(loss='categorical_crossentropy',
+    #               optimizer=optimizers.RMSprop(lr=0.00001, rho=0.9, epsilon=1e-08),
+    #               metrics=['accuracy'])
+
+    # model.compile(loss = "categorical_crossentropy",
+    #               optimizer = optimizers.SGD(lr=0.00003, momentum=0.9, nesterov=True, decay=1e-6),
+    #               metrics=["accuracy"],
+    #               decay=0.0005)
+
+    # model.compile(loss = "mean_absolute_error",
+    #               optimizer = optimizers.SGD(lr=0.0001, momentum=0.9, nesterov=True),
+    #               metrics=["accuracy"],
+    #               decay=0.0005)
+
+    model.compile(loss='mean_squared_error',
+                  optimizer=optimizers.Nadam(lr=0.0001),
+                  metrics=['accuracy'])
+
+    print_summary(model)
+    plot_model(model, to_file='model.pdf', show_shapes=True)
+
+    # ===== first stage
+    history = model.fit_generator(
+        kir_train_generator(img_dir=img_dir, scale_dir=scale_dir, batch_size=batch_size),
+        steps_per_epoch=200,
+        epochs=epochs1,
+        validation_data=kir_train_generator(img_dir=val_dir, scale_dir=scale_dir, batch_size=batch_size),
+        validation_steps=20,
+        initial_epoch=0,
+        pickle_safe=True,
+        verbose=1,
+        callbacks=[checkpoint, tensorboard])
+
+    kir_save_history(history, 'scale_pre')
+
+    # ==== second stage
+    # Unfreeze the first 6 layers
+    for layer in model.layers[:7]:
+        layer.trainable = True
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizers.Nadam(lr=0.000001),
+                  metrics=['accuracy'])
+
+    # history = model.fit_generator(
+    #         train_generator,
+    #         steps_per_epoch=1000,
+    #         epochs=epochs2,
+    #         validation_data=validation_generator,
+    #         validation_steps=100,
+    #         workers=2,
+    #         initial_epoch=epochs1,
+    #         pickle_safe=True,
+    #         verbose=2,
+    #         callbacks=[checkpoint, tensorboard])
+    #
+    # save_history(history, 'post')
+else:
+    print('Resume training after %s\'s epoch' % resumeEpochFrom)
+    model = load_model(resumeFrom)
+
+    # Unfreeze the first 5 layers
+    for layer in model.layers[:16]:
+        layer.trainable = True
+
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=optimizers.SGD(lr=0.000001, momentum=0.9, nesterov=True, decay=1e-6),
+                  metrics=["accuracy"],
+                  decay=0.0005)
+    # model.compile(loss='categorical_crossentropy',
+    #               optimizer=optimizers.Adadelta(lr=0.05, rho=0.95, epsilon=1e-08, decay=0.0),
+    #               metrics=['accuracy'])
+
+    # history = model.fit_generator(
+    #         train_generator,
+    #         steps_per_epoch=1000,
+    #         epochs=resumeEpochs,
+    #         validation_data=validation_generator,
+    #         validation_steps=50,
+    #         workers=2,
+    #         initial_epoch=resumeEpochFrom,
+    #         pickle_safe=True,
+    #         verbose=2,
+    #         callbacks=[checkpoint, tensorboard])
+    #
+    # save_history(history, 'post')
