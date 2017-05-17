850593149b260e96caa56a0b6d4f502944a86725
Kirill Makukhin <info@webage.net.au>
2017-05-17 19:48:37 +1000

    Fixed OOM problem with continue training. load_model takes much more memory than if load a fresh Resnet followed by load_weights

--- TrainScale.py
+++ TrainScale.py
@@ -28,10 +28,10 @@ batch_size = 16
 epochs1    = 40
 epochs2    = 80
 
-resumeFrom=None
-#resumeFrom='cp/crops-58-0.44.hdf5'  # <= this will trigger resuming
-resumeEpochFrom=80                   # the last trained. Will resume from resumeEpochFrom+1
-resumeEpochs=120
+# resumeFrom=None
+resumeFrom='cp/scale-06-0.17.hdf5'  # <= this will trigger resuming
+resumeEpochFrom=40                   # the last trained. Will resume from resumeEpochFrom+1
+resumeEpochs=100
 
 img_dir   = "train.scale.224"
 val_dir   = "train.scale.224.val"
@@ -116,54 +116,56 @@ tensorboard = TensorBoard(log_dir='./logs/scale-{}'.format(times),
 # early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
 
 # ========= Model ==========
-if resumeFrom == None:
 
-    # model_pretrained = applications.VGG16(weights ="imagenet", include_top=False, input_shape=(img_width, img_height, 3))
-    # print('VGG Model loaded.')
+# model_pretrained = applications.VGG16(weights ="imagenet", include_top=False, input_shape=(img_width, img_height, 3))
+# print('VGG Model loaded.')
 
-    model_pretrained = applications.ResNet50(weights ="imagenet", include_top=False, input_shape=(img_width, img_height, 3))
-    print('ResNet50 Model loaded.')
+model_pretrained = applications.ResNet50(weights ="imagenet", include_top=False, input_shape=(img_width, img_height, 3))
+print('ResNet50 Model loaded.')
 
-    pretr_layer_outputs = {}
-    for layer in model_pretrained.layers:
-        layer.trainable = False
-        pretr_layer_outputs[layer.name] = layer.get_output_at(0)
+pretr_layer_outputs = {}
+for layer in model_pretrained.layers:
+    layer.trainable = False
+    pretr_layer_outputs[layer.name] = layer.get_output_at(0)
 
-    # freeze training for a few bottom layers
-    # for layer in model_pretrained.layers[:39]:
-    #     layer.trainable = False
-        # if layer.name == 'activation_10':
-        #     break
+# freeze training for a few bottom layers
+# for layer in model_pretrained.layers[:39]:
+#     layer.trainable = False
+    # if layer.name == 'activation_10':
+    #     break
 
-    if model_pretrained.name == 'resnet50':
-        x = pretr_layer_outputs['activation_49']       # we need to skip the very last layer in case of ResNet
-    else:
-        x = model_pretrained.output
+if model_pretrained.name == 'resnet50':
+    x = pretr_layer_outputs['activation_49']       # we need to skip the very last layer in case of ResNet
+else:
+    x = model_pretrained.output
 
-    # model_pretrained.summary()
+# model_pretrained.summary()
 
-    x = Conv2D(1024, (7, 7), activation='elu', padding='valid', name='Kir_0')(x)
-    # x = AvgPool2D(pool_size=(7, 7), padding='same', name='Kir_0')(x)
-    # x = MaxPool2D(pool_size=(7, 7), padding='same', name='Kir_0')(x)
-    x = Dropout(0.5)(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(512, (1, 1), activation='elu', padding='valid', name='Kir_2')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(1, (1, 1), activation='relu', padding='valid', name='Kir_3')(x)
-    predictions = Reshape(target_shape=(1,))(x)
+x = Conv2D(1024, (7, 7), activation='elu', padding='valid', name='Kir_0')(x)
+# x = AvgPool2D(pool_size=(7, 7), padding='same', name='Kir_0')(x)
+# x = MaxPool2D(pool_size=(7, 7), padding='same', name='Kir_0')(x)
+x = Dropout(0.5)(x)
+x = BatchNormalization()(x)
+x = Conv2D(512, (1, 1), activation='elu', padding='valid', name='Kir_2')(x)
+x = BatchNormalization()(x)
+x = Conv2D(1, (1, 1), activation='relu', padding='valid', name='Kir_3')(x)
+predictions = Reshape(target_shape=(1,))(x)
 
-    # x = Flatten()(x)
-    # x = Dropout(0.2)(x)
-    # x = BatchNormalization()(x)
-    # x = Dense(512, activation='elu', name='Kir_FC1')(x)
-    # x = BatchNormalization()(x)
-    # x = Dense(256, activation='elu', name='Kir_FC2')(x)
-    # predictions = Dense(1, activation='linear',
-    #                     kernel_regularizer=regularizers.l2(0.01),
-    #                     activity_regularizer=regularizers.l1(0.01),
-    #                     name='Kir_FC3')(x)
+# x = Flatten()(x)
+# x = Dropout(0.2)(x)
+# x = BatchNormalization()(x)
+# x = Dense(512, activation='elu', name='Kir_FC1')(x)
+# x = BatchNormalization()(x)
+# x = Dense(256, activation='elu', name='Kir_FC2')(x)
+# predictions = Dense(1, activation='linear',
+#                     kernel_regularizer=regularizers.l2(0.01),
+#                     activity_regularizer=regularizers.l1(0.01),
+#                     name='Kir_FC3')(x)
 
-    model = Model(model_pretrained.input, predictions)
+model = Model(model_pretrained.input, predictions)
+if resumeFrom is not None:
+    model.load_weights(resumeFrom)
+
     # model = Model(input=model_pretrained.input, output=predictions)
 
     # model.compile(loss = "mean_absolute_error",
@@ -176,6 +178,8 @@ if resumeFrom == None:
     #               metrics=["accuracy"],
     #               decay=0.0005)
 
+
+if resumeFrom is None:
     model.compile(loss='mean_squared_error',
                   optimizer=optimizers.Nadam(lr=0.0001),
                   metrics=['accuracy'])
@@ -183,7 +187,6 @@ if resumeFrom == None:
     print_summary(model)
     plot_model(model, to_file='model.pdf', show_shapes=True)
 
-    # ===== first stage
     history = model.fit_generator(
         kir_train_generator(img_dir=img_dir, scale_dir=scale_dir, batch_size=batch_size),
         steps_per_epoch=200,
@@ -194,44 +197,14 @@ if resumeFrom == None:
         pickle_safe=True,
         verbose=1,
         callbacks=[checkpoint, tensorboard])
-
-    kir_save_history(history, 'scale_pre')
-
-    # ==== second stage
-    # Unfreeze the first 6 layers
-    # for layer in model.layers[:7]:
-    #     layer.trainable = True
-    #
-    # model.compile(loss='categorical_crossentropy',
-    #               optimizer=optimizers.Nadam(lr=0.000001),
-    #               metrics=['accuracy'])
-
-    # history = model.fit_generator(
-    #         train_generator,
-    #         steps_per_epoch=1000,
-    #         epochs=epochs2,
-    #         validation_data=validation_generator,
-    #         validation_steps=100,
-    #         workers=2,
-    #         initial_epoch=epochs1,
-    #         pickle_safe=True,
-    #         verbose=2,
-    #         callbacks=[checkpoint, tensorboard])
-    #
-    # save_history(history, 'post')
 else:
-    print('Resume training after %s\'s epoch' % resumeEpochFrom)
-    model = load_model(resumeFrom)
+    model.compile(loss='mean_squared_error',
+                      optimizer=optimizers.Nadam(lr=0.00001),
+                      metrics=['accuracy'])
 
-    # Unfreeze all layers
-    for layer in model.layers:
-        layer.trainable = True
+    print_summary(model)
+    plot_model(model, to_file='model.pdf', show_shapes=True)
 
-    model.compile(loss='mean_absolute_error',
-                  optimizer=optimizers.Nadam(lr=0.000002),
-                  metrics=['accuracy'])
-
-    # Continue training
     history = model.fit_generator(
         kir_train_generator(img_dir=img_dir, scale_dir=scale_dir, batch_size=batch_size),
         steps_per_epoch=200,
