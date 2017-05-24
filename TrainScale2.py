7e2cb5687e950b636a3fbe1a4552260348b2a464
Kirill Makukhin <info@webage.net.au>
2017-05-24 19:01:57 +1000

    TrainScale2 learns quite well

--- TrainScale2.py
+++ TrainScale2.py
@@ -1,19 +1,17 @@
 from keras import applications
-from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.models import Model, load_model
-from keras.layers import Dropout, Conv2D, Reshape, Dense, Flatten
+from keras.layers import Dropout, Conv2D, Reshape, Dense, Flatten, AvgPool2D
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
 from keras.utils import plot_model
 from keras.utils.layer_utils import print_summary
 from keras.layers.normalization import BatchNormalization
-from keras.layers.pooling import MaxPool2D, AvgPool2D
-from keras import regularizers
-from keras.preprocessing.image import load_img, img_to_array
-from keras.applications.resnet50 import preprocess_input
-import sealiondata
 from keras.applications.resnet50 import ResNet50, preprocess_input as preprocess_input_resnet
 
+import keras.backend.tensorflow_backend as K
+from sklearn.metrics import r2_score
+
+import sealiondata
 
 from time import gmtime, strftime
 
@@ -26,25 +24,22 @@ import matplotlib.pyplot as plt
 from DatasetScale import ScaleDataset
 from kir_utils import kir_save_history
 
-build = 30
+build = 35
 
 # === Train a small conv net on top of vgg16 to detect lions' scale ===
 
 batch_size = 16
 epochs1    = 40
-epochs2    = 80
+epochs2    = 120
 
-# resumeFrom=None
-resumeFrom='cp/scale-06-0.17.hdf5'  # <= this will trigger resuming
-resumeEpochFrom=40                   # the last trained. Will resume from resumeEpochFrom+1
-resumeEpochs=100
+resumeFrom=None
+# resumeFrom='cp/31checkpoint-26-0.19.hdf5'  # <= this will trigger resuming
+# resumeEpochFrom=40                   # the last trained. Will resume from resumeEpochFrom+1
+# resumeEpochs=100
 
-img_dir   = "train.scale.224"
-val_dir   = "train.scale.224.val"
 scale_dir = "../Sealion/TrainScale/"
 
-# img_width, img_height = 112, 112
-img_width, img_height = 224, 224
+img_width, img_height = 224, 224        # we also set this in ScaleDataset !!!
 
 dataset = ScaleDataset(sealiondata.SeaLionData(), preprocess_input=preprocess_input_resnet, use_categorical=False)
 times = strftime("%Y%m%d-%H-%M-%S", gmtime())
@@ -60,7 +55,7 @@ times = strftime("%Y%m%d-%H-%M-%S", gmtime())
 #     os.mkdir(out_dir)
 
 # Save the model according to the conditions
-checkpoint = ModelCheckpoint(filepath='cp/scale-{}.hdf5'.format(build),
+checkpoint = ModelCheckpoint(filepath='cp/{}'.format(build)+'checkpoint-{epoch:02d}-{val_loss:.2f}.hdf5',
                              monitor='val_acc',
                              verbose=0,
                              save_best_only=True,
@@ -73,7 +68,14 @@ tensorboard = TensorBoard(log_dir='./logs/scale-{}-{}'.format( build,times),
                           write_graph=False,
                           write_images=True)
 
-# early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+
+
+def r2_metrics(y_true, y_pred):
+	ssr = K.mean((y_pred - y_true)**2, axis=0)
+	sst = K.mean((y_pred - K.mean(K.mean(y_true)))**2,  axis=0)
+	return 1 - K.mean(ssr/sst , axis=-1)
+
 
 # ========= Model ==========
 
@@ -85,12 +87,12 @@ print('ResNet50 Model loaded.')
 
 pretr_layer_outputs = {}
 for layer in model_pretrained.layers:
-    layer.trainable = False
+    # layer.trainable = False
     pretr_layer_outputs[layer.name] = layer.get_output_at(0)
 
 # freeze training for a few bottom layers
-# for layer in model_pretrained.layers[:39]:
-#     layer.trainable = False
+for layer in model_pretrained.layers[:39]:
+    layer.trainable = False
     # if layer.name == 'activation_10':
     #     break
 
@@ -101,12 +103,14 @@ else:
 
 # model_pretrained.summary()
 
-x = Conv2D(512, (7, 7), activation='elu', padding='valid', name='Kir_0')(x)
-# x = AvgPool2D(pool_size=(7, 7), padding='same', name='Kir_0')(x)
-# x = MaxPool2D(pool_size=(7, 7), padding='same', name='Kir_0')(x)
-x = Dropout(0.5)(x)
+x = Conv2D(256, (7, 7), activation='elu', padding='valid', name='Kir_0')(x)
+
+# x = AvgPool2D(pool_size=(7, 7), padding='same', name='Kir_Pool0')(x)
+# x = Conv2D(2048, (1, 1), activation='elu', padding='valid', name='Kir_0')(x)
+
+# x = Dropout(0.5)(x)
 x = BatchNormalization()(x)
-x = Conv2D(256, (1, 1), activation='elu', padding='valid', name='Kir_2')(x)
+x = Conv2D(1024, (1, 1), activation='elu', padding='valid', name='Kir_2')(x)
 x = BatchNormalization()(x)
 x = Conv2D(1, (1, 1), activation='relu', padding='valid', name='Kir_3')(x)
 predictions = Reshape(target_shape=(1,))(x)
@@ -114,9 +118,10 @@ predictions = Reshape(target_shape=(1,))(x)
 model = Model(model_pretrained.input, predictions)
 
 if resumeFrom is None:
-    model.compile(loss='mean_absolute_error',
-                  optimizer=optimizers.Nadam(lr=0.0001),
-                  metrics=['accuracy'])
+    model.compile(loss='mse',
+                  optimizer=optimizers.Nadam(lr=0.000005),
+                  metrics=[r2_metrics])
+  				  # или исп  r2_score(y_true, y_pred, multioutput='variance_weighted')
 
     print_summary(model)
     plot_model(model, to_file='model.pdf', show_shapes=True)
@@ -132,9 +137,11 @@ if resumeFrom is None:
         verbose=1,
         callbacks=[checkpoint, tensorboard])
 else:
+    model.load_weights(resumeFrom)
     model.compile(loss='mean_absolute_error',        # mean_squared_error
-                      optimizer=optimizers.Nadam(lr=0.00001),
-                      metrics=['accuracy'])
+                      optimizer=optimizers.Nadam(lr=0.000005),
+                      metrics=['accuracy'],
+                      decay=0.0005)
 
     print_summary(model)
     plot_model(model, to_file='model.pdf', show_shapes=True)
@@ -142,10 +149,10 @@ else:
     history = model.fit_generator(
         dataset.generate(batch_size=batch_size, is_training=True),
         steps_per_epoch=200,
-        epochs=epochs1,
+        epochs=epochs2,
+        initial_epoch=epochs1,
         validation_data=dataset.generate(batch_size=batch_size, is_training=False),
         validation_steps=len(dataset.test_items),
-        initial_epoch=0,
         pickle_safe=True,
         verbose=1,
         callbacks=[checkpoint, tensorboard])
